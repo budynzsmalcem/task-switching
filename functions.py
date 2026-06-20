58900ef834d27c99f1c64b6cb991efc7b2f8ce7d
Tomasz Pytlos <74183055+budynzsmalcem@users.noreply.github.com>
2026-06-20 16:10:32 +0200

    Update functions.py

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ from psychopy import visual, core
 from psychopy.hardware import keyboard
 import random
 
+#parameter import from yaml file
 def load_config(file_name="config.yaml"):
     with open(file_name, encoding="utf-8") as yaml_file:
         config = yaml.safe_load(yaml_file)
