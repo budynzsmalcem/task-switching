2d547d576c7649f2bcb86f1160b3e9d719070bbe
Tomasz Pytlos <74183055+budynzsmalcem@users.noreply.github.com>
2026-06-20 16:39:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from psychopy import visual, core
 from psychopy.hardware import keyboard
 
-import bodzce1feedback import trial
+import bodzce1feedback import *
 
 if __name__ == "__main__":
     trial()
