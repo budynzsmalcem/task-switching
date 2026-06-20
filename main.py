8a9506ab60ff0a694072e9b6b8a24f5f36a04cca
Tomasz Pytlos <74183055+budynzsmalcem@users.noreply.github.com>
2026-06-20 16:40:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from psychopy import visual, core
 from psychopy.hardware import keyboard
 
 import bodzce1feedback import *
+_config_defaults = load_config()
 
 if __name__ == "__main__":
-    trial()
+    trial(_config_defaults[""])
     win.close()
     core.quit()
