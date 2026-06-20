d87da20f907a94f9413f7cdecb0f6c0d0e1cc867
Tomasz Pytlos <74183055+budynzsmalcem@users.noreply.github.com>
2026-06-20 16:30:09 +0200

    Update bodzce1feedback.py

--- bodzce1feedback.py
+++ bodzce1feedback.py
@@ -8,47 +8,42 @@ import random
 global_trial_number = 0
 
 # Załadowanie ustawień z pliku .yaml: funkcja load_config
-def load_config(file_name):
-    with open(file_name, encoding="utf-8") as yaml_file:
-        config = yaml.safe_load(yaml_file)
-    return config
 
 
 win = visual.Window(
-    size=[1024, 768],
-    fullscr=False,
-    color='black',
-    units='pix',
-    checkTiming=False
+    size=_config_defaults["screen_size"],
+    fullscr=_config_defaults["fullscr"],
+    color=_config_defaults["screen_color"],
+    units=_config_defaults["unit"],
+    checkTiming=_config_defaults["checkTiming"]
 )
 kb = keyboard.Keyboard()
 
-COLOR_BLUE = '#44EDFF'
-COLOR_GREEN = '#E2FF55'
+stim_bc = visual.Circle(win, radius=_config_defaults["circle_radius_1"], fillColor=_config_defaults["circle_color_1"], lineColor=None)
+stim_gc = visual.Circle(win, radius=_config_defaults["circle_radius_2"], fillColor=_config_defaults["circle_color_2"], lineColor=None)
 
-stim_bc = visual.Circle(win, radius=60, fillColor=COLOR_BLUE, lineColor=None)
-stim_gc = visual.Circle(win, radius=60, fillColor=COLOR_GREEN, lineColor=None)
-
-stim_bs = visual.Rect(win, width=120, height=120, fillColor=COLOR_BLUE, lineColor=None)
-stim_gs = visual.Rect(win, width=120, height=120, fillColor=COLOR_GREEN, lineColor=None)
+stim_bs = visual.Rect(win, width=_config_defaults["square_size_1"], height=_config_defaults["square_size_1"], fillColor=_config_defaults["square_color_1"], lineColor=None)
+stim_gs = visual.Rect(win, width=_config_defaults["square_size_2"], height=_config_defaults["square_size_2"], fillColor=_config_defaults["square_color_2"], lineColor=None)
 
 stim_list = [stim_bc, stim_gc, stim_bs, stim_gs]
-# Poprawne odpowiedzi
+
 #Punkt fiksacji (biały krzyżyk)
-fixation = visual.TextStim(win, text='+', color='white', height=20)
+fixation = visual.TextStim(win, text='+', color=_config_defaults["fixation_point_color"], height = _config_defaults["fixation_point_height"])
 
 #  Wskazówki
-cue_bg = visual.Rect(win, width=200, height=80, fillColor='#DEDEDE', lineColor='#B0B0B0', lineWidth=6)
-cue_text_color = visual.TextStim(win, text='KOLOR', color='black', height=32, bold=True)
-cue_text_shape = visual.TextStim(win, text='KSZTAŁT', color='black', height=32, bold=True)
+cue_bg = visual.Rect(win, width=_config_defaults["cue_bg_width"], height=_config_defaults["cue_bg_height]", fillColor=_config_defaults["cue_bg_fillColor"], lineColor=_config_defaults["cue_bg_lineColor"], lineWidth=_config_defaults["cue_bg_lineWidth"])
+cue_text_color = visual.TextStim(win, text='KOLOR', color=_config_defaults["cue_text_color_text_color"], height=_config_defaults["cue_text_color_height"], bold=_config_defaults["cue_text_color_isBold"])
+cue_text_shape = visual.TextStim(win, text='KSZTAŁT', color=_config_defaults["cue_text_shape_text_color"], height=_config_defaults["cue_text_shape_height"], bold=_config_defaults["cue_shape_color_isBold"])
 
-#  Feedback (nie wiem tu mam propozycje czata zeby feedback byl tekstem, ale nie wiem, mozna przerobic na jpg)
-feedback_correct = visual.TextStim(win, text='✓', color='grey', height=100)
-feedback_incorrect = visual.TextStim(win, text='✗', color='grey', height=100)
-feedback_timeout = visual.TextStim(win, text='ZA WOLNO', color='grey', height=40)
+#  Feedback 
+feedback_correct = visual.TextStim(win, text='✓', color=_config_defaults["feedback_correct_color"], height=_config_defaults["feedback_correct_height"])
+feedback_incorrect = visual.TextStim(win, text='✗', color=_config_defaults["feedback_incorrect_color"], height=_config_defaults["feedback_incorrect_height"])
+feedback_timeout = visual.TextStim(win, text='ZA WOLNO', color=_config_defaults["feedback_timeout_color"], height=_config_defaults["feedback_timeout_height"])
 
-answers_color = {stim_bc: "k", stim_gc: "d", stim_bs: "k", stim_gs: "d"}
-answers_shape = {stim_bc: "d", stim_gc: "d", stim_bs: "k", stim_gs: "k"}
+# Poprawne odpowiedzi
+answers_color = {stim_bc: _config_defaults["key_2"], stim_gc: _config_defaults["key_1"], stim_bs: _config_defaults["key_2"], stim_gs: _config_defaults["key_1"]}
+answers_shape = {stim_bc: _config_defaults["key_1"], stim_gc: _config_defaults["key_1"], stim_bs: _config_defaults["key_2"], stim_gs: _config_defaults["key_2"]}
+
 
 
 
